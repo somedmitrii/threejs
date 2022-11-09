c358bc836f6e11722bff53bbb49fd6e86c4362a5
somedmitrii <somedmitrii@gmail.com>
2022-11-09 23:39:39 +0400

    add bushes in house

--- src/house/index.js
+++ src/house/index.js
@@ -3,9 +3,11 @@ import * as THREE from "three";
 import { door } from "./door";
 import { walls } from "./walls";
 import { roof } from "./roof";
+import { bush1, bush2, bush3, bush4 } from "../bushes";
 
 export const house = new THREE.Group();
 
 house.add(walls);
 house.add(roof);
 house.add(door);
+house.add(bush1, bush2, bush3, bush4);
